26eb3fc523318095594c9e2fe34a938fa53c56af
likely <532837139@qq.com>
2016-08-05 17:43:33 +0800

    ...

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM,{render} from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 require("../less/style.less")
 
-ReactDOM.render((
+render(
   <Router
     history={browserHistory}
     routes={routes}
   />
-), document.getElementById('app'))
+, document.getElementById('app'))
